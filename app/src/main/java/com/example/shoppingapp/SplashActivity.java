9dddd9e9d99ff5250246d50bdf8d6bc422d187fb
Khairul Anam Mubin <anam.mubin@gmail.com>
2021-07-14 14:54:01 +0600

    app icon, app name, first page img btn removed and added 3s delay, color changes, and some minor bug fixed in different files

--- app/src/main/java/com/example/shoppingapp/SplashActivity.java
+++ app/src/main/java/com/example/shoppingapp/SplashActivity.java
@@ -14,19 +14,34 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class SplashActivity extends AppCompatActivity
 {
     //Animation topA,bottomA;
-    ImageView img;
-    TextView s_title,title_txt;
-
+    //ImageView img;
+    //TextView s_title,title_txt;
+    Timer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
-        img = (ImageView) findViewById(R.id.splash_img);
+        // Adding delay timer to jump to mainActivity
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                //Toast.makeText(SplashActivity.this, "Welcome to Prime eShop", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+//        img = (ImageView) findViewById(R.id.splash_img);
 
 //        topA = AnimationUtils.loadAnimation(this,R.anim.top_animation);
 //        bottomA = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
@@ -39,15 +54,17 @@ public class SplashActivity extends AppCompatActivity
 //        title_txt.setAnimation(bottomA);
 //        SystemClock.sleep(4000);
 
-        img.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
-                startActivity(mainIntent);
-                Toast.makeText(SplashActivity.this, "Welcome to Compras BD", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        });
+//        img.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+//                startActivity(mainIntent);
+//                Toast.makeText(SplashActivity.this, "Welcome to Compras BD", Toast.LENGTH_SHORT).show();
+//                finish();
+//            }
+//        });
+
+
 
     }
 }
